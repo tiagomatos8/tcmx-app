4a221b3b23105e38eabf58857ecd25284645fc2c
Tiago C M Matos <tiago@ananasacademy.com>
2019-10-03 22:55:24 +1000

    tech skills section added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <section className="hero is-fullheight is-primary has-background">
@@ -52,11 +52,142 @@ function App() {
         </section>
         </div>
       </section>
-      <body>
         <section class="section">
           <div class="container">
             <div class="heading">
-              <h1 class="title">TECHNICAL SKILLS</h1>
+              <div class="title has-text-centered">TECHNICAL SKILLS</div>
+            </div>
+          </div>
+        </section>
+        <section class="section">
+          <div class="container">
+            <div class="heading">
+              <div class="subtitle has-text-centered"><strong>Front-End</strong></div>
+            </div>
+            <div className="columns">
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">React</p>
+                    <p className="subtitle">Tiago uses atomic design approach to break entire interfaces down into smaller components.</p>
+                    <progress class="progress" value="80" max="100">80%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">SCSS/CSS</p>
+                    <p className="subtitle">Use of BEM naming convention to help with reusability of independent blocks.</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">Javascript ES8</p>
+                    <p className="subtitle">Fetch API, HTML Drag and Drop API, Media Capture and Streams API, Touch events and more</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div class="heading">
+              <div class="subtitle has-text-centered"><strong>Back-End</strong></div>
+            </div>
+            <div className="columns">
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">NODEJS</p>
+                    <p className="subtitle">akbfk bkafb kabfk bakub kuab kufbak ub kbuakub kuab kuak ubakbu ka buaku</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">JAVA</p>
+                    <p className="subtitle">akbfk bkafb kabfk bakub kuab kufbak ub kbuakub kuab kuak ubakbu ka buaku</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">GRAPHQL</p>
+                    <p className="subtitle">akbfk bkafb kabfk bakub kuab kufbak ub kbuakub kuab kuak ubakbu ka buaku</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="columns">
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">SASS</p>
+                    <p className="subtitle">Use of BEM naming convention to help with reusability of independent blocks.</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">SASS</p>
+                    <p className="subtitle">Use of BEM naming convention to help with reusability of independent blocks.</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">SASS</p>
+                    <p className="subtitle">Use of BEM naming convention to help with reusability of independent blocks.</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="columns">
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">SASS</p>
+                    <p className="subtitle">Use of BEM naming convention to help with reusability of independent blocks.</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">SASS</p>
+                    <p className="subtitle">Use of BEM naming convention to help with reusability of independent blocks.</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <p className="title">SASS</p>
+                    <p className="subtitle">Use of BEM naming convention to help with reusability of independent blocks.</p>
+                    <progress class="progress" value="70" max="100">70%</progress>
+                  </div>
+                </div>
+              </div>
             </div>
             <div class="table-container">
               <table class="table is-bordered">
@@ -69,7 +200,15 @@ function App() {
             </div>
           </div>
         </section>
-      </body>
+        <div class="container">
+            <div class="columns is-centered">
+              <div class="column">
+                <div className="box">
+                  Login
+                </div>
+              </div>
+            </div>
+        </div>
     </div>
   );
 }
